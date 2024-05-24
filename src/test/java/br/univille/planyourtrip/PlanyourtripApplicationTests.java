028ff45f0929845bad088f2c9c918e107135e350
NathanBergmann <nathan.bergmann@agapys.com>
2024-05-23 21:35:51 -0300

    First Commit
    Start Project

---
+++ src/test/java/br/univille/planyourtrip/PlanyourtripApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.planyourtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanyourtripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
